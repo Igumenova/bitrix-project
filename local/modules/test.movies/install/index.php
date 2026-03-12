a0b12591a6bc1b798a5bb766542ff2c3a675b1c3
Igumenova <yulya.gummi@gmail.com>
2026-03-12 11:29:27 +0300

    fixed gitignore

---
+++ local/modules/test.movies/install/index.php
@@ -0,0 +1,29 @@
+<?php
+use Bitrix\Main\ModuleManager;
+
+class test_movies extends CModule
+{
+    public $MODULE_ID = 'test.movies';
+    public $MODULE_VERSION;
+    public $MODULE_VERSION_DATE;
+    public $MODULE_NAME = 'Фильмы';
+    public $MODULE_DESCRIPTION = 'Модуль для управления фильмами';
+
+    public function __construct()
+    {
+        $arModuleVersion = [];
+        include(__DIR__ . '/version.php');
+        $this->MODULE_VERSION = $arModuleVersion['VERSION'];
+        $this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'];
+    }
+
+    public function DoInstall()
+    {
+        ModuleManager::registerModule($this->MODULE_ID);
+    }
+
+    public function DoUninstall()
+    {
+        ModuleManager::unRegisterModule($this->MODULE_ID);
+    }
+}
